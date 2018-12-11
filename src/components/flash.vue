3dd095736c6b553611eec29b4f809f3feeec58c7
junminghou <357121200@qq.com>
2018-12-11 15:55:31 +0800

    魔法编程-优化

--- src/components/flash.vue
+++ src/components/flash.vue
@@ -10,58 +10,26 @@
 
     <div id="create_table_script" ref="create_table_script">
       -- auto-generated definition
-      CREATE TABLE st_partner_stock_month
+      CREATE TABLE user
       (
-      id INT AUTO_INCREMENT
-      COMMENT 'id'
+      user_id    BIGINT                              NOT NULL
+      COMMENT '用户ID'
       PRIMARY KEY,
-      region VARCHAR(255) NULL
-      COMMENT '大区',
-      province VARCHAR(255) NULL
-      COMMENT '省份',
-      city VARCHAR(255) NULL
-      COMMENT '城市',
-      sale_person VARCHAR(255) NULL
-      COMMENT '销售人员',
-      partner VARCHAR(255) NULL
-      COMMENT '经销商',
-      sync_date DATE NULL
-      COMMENT '同步时间',
-      date_year VARCHAR(255) NULL
-      COMMENT '年',
-      date_month VARCHAR(255) NULL
-      COMMENT '月',
-      date_quarter VARCHAR(255) NULL
-      COMMENT '季度',
-      amount_in BIGINT(16) NULL
-      COMMENT '金额（进货）',
-      num_in INT NULL
-      COMMENT '数量（进货）',
-      amount_out BIGINT(16) NULL
-      COMMENT '金额（出货）',
-      num_out BIGINT(11) NULL
-      COMMENT '数量（出货）',
-      amount_gather BIGINT(16) NULL
-      COMMENT '金额（收集）',
-      num_gather BIGINT(11) NULL
-      COMMENT '数量（收集）',
-      gmt_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL
-      COMMENT '创建时间',
-      goods_name VARCHAR(255) NULL
-      COMMENT '商品名称',
-      category_1st VARCHAR(255) NULL
-      COMMENT '商品分类一',
-      category_2nd VARCHAR(255) NULL
-      COMMENT '商品分类二',
-      category_3rd VARCHAR(255) NULL
-      COMMENT '商品分类三',
-      is_important VARCHAR(255) NULL
-      COMMENT '是否重点商品（瞬感、至新）',
-      contract_status VARCHAR(255) NULL
-      COMMENT '签约状态（签约、未签约）'
+      user_type  INT                                 NULL
+      COMMENT '用户的类型，1：主账号，2：子账号',
+      parent_id  BIGINT                              NULL
+      COMMENT '主账号ID',
+      company_id BIGINT                              NULL
+      COMMENT '企业ID',
+      gmt_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      gmt_modify TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      CONSTRAINT user_user_id_uindex
+      UNIQUE (user_id),
+
       )
-      COMMENT '经销商库存'
+      COMMENT '用户表'
       ENGINE = InnoDB;
+
 
 
     </div>
@@ -100,7 +68,7 @@
       <div>public interface {{table.pascalName}}Mapper {</div>
       <div>
         <div class="querySql">
-          String querySql = " where 1=1 " +
+          String querySql = " where 1=1 "
           <div v-for="(column,index) in table.columns">
             + " {{ _("if test = 'query."+column.camelName+" != null'") }} and {{column.name}} =
             #{query.{{column.camelName}}} {{_("/if")}} "
@@ -135,14 +103,16 @@
           <br/>
           + "{{_('set')}}"
           <template v-for="(column,index) in table.columns">
-            <br/>
+            <template v-if="column.name !== table.primaryKey ">
+              <br/>
             + " {{ _("if test = 'entity."+column.camelName+" != null'") }} {{column.name}} =
             #{entity.{{column.camelName}}}, {{_("/if")}} "
+            </template>
           </template>
           <br/>
           + "{{_('/set')}}"
           <br/>
-          + "where id = #{record.id} {{_('/script')}}")
+          + "where {{table.primaryKey}} = #{record.{{table.primaryKeyCamel}}} {{_('/script')}}")
           <br/>
           int update(@Param("entity") {{table.pascalName}} entity);
         </div>
@@ -150,20 +120,20 @@
         <div class="deleteSingle">
           @Delete("delete from {{table.name}} where id = #{id}")
           <br/>
-          int deleteById(@Param("{{table.primaryKey}}") {{table.primaryKeyType}} {{table.primaryKey}});
+          int deleteById(@Param("id") {{table.primaryKeyType}} {{table.primaryKey}});
         </div>
         <br>
         <div class="baseQuery">
           @Select("{{_('script')}} select * from {{table.name}} where id = #{id} {{ _('/script')}}")
           <br>
-          {{table.pascalName}} get{{table.pascalName}}ById(@Param("{{table.primaryKey}}") {{table.primaryKeyType}}
+          {{table.pascalName}} get{{table.pascalName}}ById(@Param("id") {{table.primaryKeyType}}
           {{table.primaryKey}});
           <br>
           <br>
           @Select("{{_('script')}} select * from {{table.name}} where {{table.primaryKey}} in " +
           " {{_("foreach collection='ids' index='index' item='item' open='(' close=')' separator=',' ")}} " +
           " #{item} " +
-          " {{_("/foreach")}} {{ _('/script')}}") ")
+          " {{_("/foreach")}} {{ _('/script')}}")
           <br>
           List<{{table.pascalName}}> get{{table.pascalName}}ByIds(@Param("ids") List<{{table.primaryKeyType}}> id);
         </div>
