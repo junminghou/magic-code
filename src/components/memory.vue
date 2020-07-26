d49233f2fcb0caaa8e1964d40a0783d1f1810b6b
junminghou <357121200@qq.com>
2020-07-26 22:06:35 +0800

    记忆闪卡 V0.2 加入快捷键操作

--- src/components/memory.vue
+++ src/components/memory.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="font-family: 微软雅黑; font-size: 30px; color: #87e8de;">
+  <div style="font-family: 微软雅黑; font-size: 30px; color: #87e8de;" >
     <div style="top: 40%; left: 40%; position: absolute;">{{show_text}}</div>
     <div style="position: absolute; bottom: 0; left: 42%; margin-bottom: 50px;">
       <div style="text-align:center;">{{percentage}}</div>
@@ -8,11 +8,29 @@
       </i-select>
       <div>
         <i-button @click="showAction(button_text)" style="width: 95px;">{{button_text}}</i-button>
-        <i-button @click="showAction('continue')" style="width: 95px;">continue</i-button>
+        <i-button @click="showAction('continue')" style="width: 95px;">continue</i-button>        
       </div>
+      <i-input style="width: 200px" placeholder="快捷键" v-model="shortcut_key" @keyup.s.native="doSomething('s')" @keyup.c.native="doSomething('c')" 
+                                    @keyup.space.native="doSomething('space')" @keyup.left.native="doSomething('left')" @keyup.right.native="doSomething('right')"
+                                    @keyup.up.native="doSomething('up')" @keyup.down.native="doSomething('down')"/>
     </div>
     <div ref="data_source" style="display: none;">
-      
+      他需要来这么早吗
+      他们需要填表吗
+      我需要来吗
+      他需要这么做吗
+      你不需要再做它了
+      你不需要来
+      他不需要知道
+      你不需要买这些东西
+      他需要知道这些
+      你们需要每天学习
+      我们需要买很多东西
+      我们需要告诉他
+      你的头发需要洗了
+      这桌子需要喷漆了
+      你的房子需要装修了
+      你的作业需要做了
     </div>
   </div>
 </template>
@@ -27,6 +45,7 @@ export default {
   },
   data() {
     return {
+      model: 1,
       percentage: "",
       show_text: "",
       button_text: "start",
@@ -34,12 +53,14 @@ export default {
       interval_second: 5,
       value: 0,
       data_source: [],
-      interval_line: 5,
-      every_line_show_times: 3,
-      model: 1,
+      interval_line: 4,
+      every_line_show_times: 3,      
       pause_points: [],
       sections: [],
-      sections_selected: null
+      sections_selected: null,
+      shortcut_key: "",
+      is_lock: false,
+      is_start: false,
     };
   },
   mounted() {
@@ -73,23 +94,26 @@ export default {
         let current_no = current_index + this.interval_line;
         current_no =
           current_no > text_array.length ? text_array.length : current_no;
-        for (let hh = 0; hh < this.every_line_show_times; hh++) {
+        let last_data = "";
+        for (let hh = 0; hh < this.every_line_show_times; hh++) {          
+          if(hh > 0){
+            latest_data.push(last_data);
+          }
           for (let j = current_index; j < current_no; j++) {
             let before = j - 1;
             let after = j + 1;
             if (before !== current_index - 1) {
-              console.log(before);
               latest_data.push(text_array[before].trim());
             }
-            console.log(j);
             if (after >= current_no) {
               after = current_index;
             }
-            console.log(after);
-            latest_data.push(text_array[j].trim());
+            last_data = text_array[j].trim();
+            latest_data.push(last_data);
             latest_data.push(text_array[after].trim());
           }
         }
+        latest_data.push(last_data);
         this.pause_points.push(latest_data.length - 1);
         this.sections.push({value: i, text: "第"+ (i + 1) +"段"});
       }
@@ -98,6 +122,7 @@ export default {
     console.log(this.pause_points);
     this.data_source = latest_data;
     this.sections_selected = 0;
+    this.set_show_text(0);
   },
   methods: {
     get() {
@@ -111,16 +136,55 @@ export default {
       if (is_pause) {
         this.showAction("pause");
       }
-      this.percentage =
-        ((this.value / (this.data_source.length - 1)) * 100).toFixed() + " %";
-      this.show_text = this.data_source[this.value];
-      this.value++;
-      if (this.value >= this.data_source.length) {
-        this.value = 0;
+      if (!this.sync_lock) {
+        this.set_show_text(current_value + 1);    
       }
+    },
+    doSomething(key_word) {
+      if(key_word == "s") {
+        this.showAction("start");
+      } else if(key_word == "c") {
+        this.showAction("continue");
+      } else if(key_word == "space") {
+        this.showAction("pause");
+      } else if (key_word == "left") {      
+        if (this.is_start == true) {    
+          this.showAction("pause");
+          this.set_show_text(this.value - 1);                        
+          this.showAction("continue");
+        } else {
+          this.set_show_text(this.value - 1);   
+        }
+      } else if (key_word == "right") {                
+         if (this.is_start == true) {    
+          this.showAction("pause");
+          this.set_show_text(this.value + 1);                        
+          this.showAction("continue");
+        } else {
+          this.set_show_text(this.value + 1);   
+        }
+      }
+      this.shortcut_key = "";
+    },
+    set_show_text(current_value) {
+      // is_timer = (is_timer == undefined ? false : is_timer);      
+      if(this.data_source == null || this.data_source.length == 0) {
+        return;
+      }
+      if (current_value < 0){
+        current_value = 0;
+      } else if (current_value > this.data_source.length - 1){
+        current_value = this.data_source.length - 1;
+      }     
+      this.value = current_value;
+      this.setter_section();
+      this.show_text = this.data_source[current_value];
+      this.percentage =
+        ((current_value / (this.data_source.length - 1)) * 100).toFixed() + " %";        
     },
     showAction(name) {
       if (name === "start") {
+        this.is_lock = false;        
         if(this.sections_selected == 0) {
             this.value = 0
         } else {
@@ -128,24 +192,31 @@ export default {
         }
         this.timer = setInterval(this.get, this.interval_second * 1000);
         this.button_text = "pause";
+        this.is_start = true;
       } else if (name === "pause") {
+        this.is_start = false;
+        this.is_lock = true;        
         clearInterval(this.timer);
-        this.button_text = "start";      
+        this.button_text = "start";        
       } else if (name === 'continue') {
-           if (this.button_text == "start") {          
-            let current_section = 0;
-            for (let i = 0; i < this.pause_points.length; i++) {
-                if(this.value <= this.pause_points[i]) {
-                    current_section = i;
-                    break;
-                }
-            }
-            this.sections_selected = current_section;
+           this.is_lock = false;        
+           if (this.button_text == "start") {                      
             this.timer = setInterval(this.get, this.interval_second * 1000);
             this.button_text = "pause";
+            this.is_start = true;
           }
       }
     },
+    setter_section() {
+      let current_section = 0;
+      for (let i = 0; i < this.pause_points.length; i++) {
+          if(this.value <= this.pause_points[i]) {
+              current_section = i;
+              break;
+          }
+      }
+      this.sections_selected = current_section;
+    }
   },
   beforeDestroy() {
     clearInterval(this.timer);
